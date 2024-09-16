9952a66d9bf894dc4f2bb10233a4f17ba7b2bddd
Byjony50 <115209449+Byjony50@users.noreply.github.com>
2024-09-16 08:09:50 +0200

    Add files via upload

---
+++ Programacion/primer-trimestre/resuelto/E06Numeros.java
@@ -0,0 +1,24 @@
+
+public class E06Numeros {
+
+	public static void main(String[] args) {
+		// DECLARACION DE VARIABLES
+		int cont;
+		
+		//Mostrar 10 veces en pantalla "hola" con bucle ascendente.
+		cont=0;
+		while(cont<10 ) {
+			System.out.println("Hola");
+			cont=cont+1;
+			
+		}
+		
+		//Mostrar 10 veces en pantalla "Adios" con bucle descendente
+		cont=10;
+		while(cont>0) {
+			System.out.println("Adios");
+			cont=cont-1;
+		}
+	}
+
+}
